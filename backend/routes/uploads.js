0873696a36a13fd4fe84918a1bd1011e58041bb9
Robert De Leon <robedeleong@gmail.com>
2021-01-31 14:45:39 -0600

    - update: backend/uploads - fixed controller

--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import * as UploadsController from '../controllers/uploadsController.js';
+import express from "express";
+import * as UploadsController from "../controllers/uploadsController.js";
 
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-    res.render('../views/index');
+router.route("/").get((req, res) => {
+    res.render("../views/index");
 });
 
-router.route("/products").post(UploadsController.uploadProduct("products").single("file"), UploadsController.uploadFile);
+router.route("/").post(UploadsController.uploadPicture("avatars").single("file"), UploadsController.uploadFile);
 
-router.route('/files').get(UploadsController.getFilesJSON);
+router.route("/files").get(UploadsController.getFilesJSON);
 
-router.route('/files/:filename').get(UploadsController.getFile);
+router.route("/files/:filename").get(UploadsController.getFile);
 
-router.route('/image/:filename').get(UploadsController.getImageFile);
+router.route("/image/:filename").get(UploadsController.getImageFile);
 
-router.route('/files/:id').delete(UploadsController.deleteFile);
+router.route("/files/:id").delete(UploadsController.deleteFile);
 
 export default router;
