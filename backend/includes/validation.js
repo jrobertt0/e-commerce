32cea6a6d1929a0136a9cf0013482984b4802376
Robert De Leon <robedeleong@gmail.com>
2021-02-03 16:53:00 -0600

    - update: backend/endpoints - added validation to item

--- backend/includes/validation.js
+++ backend/includes/validation.js
@@ -1,53 +1,44 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 export const registerValidation = (data) => {
-    
-    const schema = Joi.object({
-        username: Joi.string()
-            .min(6).max(25)
-            .required(),
-        name: Joi.string()
-            .min(6).max(25)
-            .required(),
-        email: Joi.string()
-            .min(6)
-            .required()
-            .email()
-    });
+	const schema = Joi.object({
+		username: Joi.string().min(6).max(25).required(),
+		name: Joi.string().min(6).max(25).required(),
+		email: Joi.string().min(6).required().email(),
+	});
 
-    return schema.validate(data);
-}
+	return schema.validate(data);
+};
 
 export const passwordValidation = (data) => {
-    const schema = Joi.object({
-        password: Joi.string()
-            .min(6)
-            .required()
-            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-    });
+	const schema = Joi.object({
+		password: Joi.string()
+			.min(6)
+			.required()
+			.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+	});
 
-    return schema.validate(data);
-}
+	return schema.validate(data);
+};
 
 export const loginValidation = (data) => {
-    
-    const schema = Joi.object({
-        email: Joi.string()
-            .min(6)
-            .required()
-            .email(),
-        password: Joi.string()
-            .min(6)
-            .required()
-            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    });
+	const schema = Joi.object({
+		email: Joi.string().min(6).required().email(),
+		password: Joi.string()
+			.min(6)
+			.required()
+			.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+	});
 
-    return schema.validate(data);
-}
+	return schema.validate(data);
+};
 
 export const itemValidation = (data) => {
-    // const schema = Joi.object({
-        
-    // })
-    return {};
-}
+	const schema = Joi.object({
+		name: Joi.string().min(6).required(),
+		description: Joi.string().min(6),
+		offer: Joi.string(),
+		category: Joi.array(),
+	});
+	return schema.validate(data);
+};
